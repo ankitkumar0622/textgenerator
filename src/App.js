86c63d2adcb51e647b6cd716b6c161c590cd5d74
Ankit <ankit.kumar730007@gmail.com>
2022-09-18 17:19:26 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React , { useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [quotes, setQuotes] = useState("");
+
+ // const textRef = useRef();
+  //let colors = ["#ffff00", "#90ee90" , "#ffa500", "#ff68ff", "#a9a9e7"];
+
+
+  const getQuote = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) =>res.json())
+    .then((data) => {
+      let randomNum = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomNum]);
+    });
+  };
+
+
+//   useEffect( () => {
+//   textRef.current.style.color =
+//   colors[Math.floor(Math.random() * colors.length)];
+//  }, [quotes]);
+
+
+
+return(
+  <div className="App">
+    <div className="quote">
+    <h2>ID: {quotes.id}</h2>
+    <p>Body: {quotes.body}</p>
+     <button className="btn" onClick={getQuote}>Get Quote</button>
+  </div>
+  </div>
+);
+};
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
